b01014db6d708460ac77cf6b1e1d88ee8f05b036
Your Name <mollenwambui@gmail.com>
2022-06-10 20:32:16 +0300

    first commit

--- bank.py
+++ bank.py
@@ -1,15 +1,46 @@
 class Bank:
-    def __init__(self,name,number,balance,bankname) :
+    def __init__(self,name,number,bankname) :
         self.name=name
         self.number=number
-        self.balance=balance
+        self.balance=0
         self.bankname=bankname
+        self.deposits=[]
+        self.withdrawals=[]
+
+
       
 
     def deposit(self,amount):
-        self.balance+=amount
-        return f"Hello your current balance is {self.balance} "
+        if amount<=0:
+            return f"Deposit should be greater than 0"
+        else:
+          self.balance+=amount
+          self.deposits.append(amount)
+        return f"Hello  you have sucessfully deposited {amount} your current balance is {self.balance} "
 
     def  withdraw(self,amount) :
-        self.balance-=amount 
-        return f"Hello your current balance is {self.balance}"
+        if amount<=self.balance and amount>0:
+          transactions=amount+100
+          self.balance-=transactions
+          self.withdrawals.append(amount)
+        else:
+            return "withdrawal failed"  
+        return f"Hello you have withdrawn {amount}current balance is {self.balance}"
+
+    def deposit_statements(self):
+         for x in self.deposits:
+             print(f"Hello you have deposited {x} ",end="\n")
+             
+        
+
+    def withdrawals_statement(self):
+         for x in self.withdrawals:
+             print(f"Hello you have withdrawn {x} ",end="\n")
+   
+
+    def current_balance(self):
+        return f"Hello your current balance is  {self.balance} "
+
+
+
+             
